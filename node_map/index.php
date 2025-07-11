9ff36a859ec089d8763dd42aa7bc22631b0a825e
Your Name <your_email@example.com>
2025-07-11 15:36:10 +0800

    去掉初始化

--- node_map/index.php
+++ node_map/index.php
@@ -215,8 +215,7 @@
         // Zoho Creator 数据获取和填充下拉菜单
         window.addEventListener('load', async function () {
             try {
-                // 确保 Zoho Creator SDK 已初始化
-                await ZOHO.CREATOR.init();
+                
 
                 const config = {
                     app_name: '-demo', // 替换为你的应用名称
